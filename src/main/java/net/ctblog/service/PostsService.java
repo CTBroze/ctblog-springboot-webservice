4ecb421724984fdb43360f0d78ca56712047e2d0
CTBroze <ctbroze@gmail.com>
2019-12-26 23:47:37 +0900

    Post등록 및 수정 삭제 기능 실습

--- src/main/java/net/ctblog/service/PostsService.java
+++ src/main/java/net/ctblog/service/PostsService.java
@@ -2,6 +2,7 @@ package net.ctblog.service;
 
 import lombok.RequiredArgsConstructor;
 import net.ctblog.domain.posts.Posts;
+import net.ctblog.web.dto.PostsListResponseDto;
 import net.ctblog.web.dto.PostsResponseDto;
 import net.ctblog.web.dto.PostsUpdateRequestDto;
 import org.springframework.stereotype.Service;
@@ -9,6 +10,9 @@ import org.springframework.transaction.annotation.Transactional;
 
 import net.ctblog.domain.posts.PostsRepository;
 import net.ctblog.web.dto.PostsSaveRequestDto;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 
 @RequiredArgsConstructor
@@ -27,6 +31,20 @@ public class PostsService {
         posts.update(requestDto.getTitle(), requestDto.getContent());
 
         return id;
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new) // .map(posts -> new PostsListResponseDto(posts))와 동일
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete(Long id){
+        Posts posts = postsRepository.findById(id).orElseThrow(()->new IllegalArgumentException("해당게시글이 없습니다. id="+id));
+
+        postsRepository.delete(posts); //JPARepository에서 delete를 지원하므로 활용한다.
     }
 
     public PostsResponseDto findById (Long id){
--- src/main/java/net/ctblog/web/dto/PostsListResponseDto.java
+++ src/main/java/net/ctblog/web/dto/PostsListResponseDto.java
@@ -1,19 +1,24 @@
 package net.ctblog.web.dto;
 
 import lombok.Getter;
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import net.ctblog.domain.posts.Posts;
 
+import java.time.LocalDateTime;
+
+
 @Getter
-public class PostsResponseDto {
+public class PostsListResponseDto {
     private Long id;
     private String title;
-    private String content;
     private String author;
+    private LocalDateTime modifiedDate;
 
-    public PostsResponseDto(Posts entity){
+    public PostsListResponseDto(Posts entity){
         this.id = entity.getId();
         this.title = entity.getTitle();
-        this.content = entity.getContent();
         this.author = entity.getAuthor();
+        this.modifiedDate = entity.getModifiedDate();
     }
 }
