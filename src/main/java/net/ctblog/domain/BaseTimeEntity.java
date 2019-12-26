420fff9a2069dc4db852bc5d3caa9a7d543d9ef7
CTBroze <ctbroze@gmail.com>
2019-12-26 18:30:54 +0900

    생성시간 및 수정시간 저장을 위한 BaseTimeEntity작성

---
+++ src/main/java/net/ctblog/domain/BaseTimeEntity.java
@@ -0,0 +1,22 @@
+package net.ctblog.domain;
+
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass //JPA Entity클래스들이 이 클래스를 상속할 경우 필드들도 칼럼으로 인식하도록 한다.
+@EntityListeners(AuditingEntityListener.class) //이 클래스에 Auditing기능을 포함한다
+public class BaseTimeEntity {
+
+    @CreatedDate //Entity가 생성될때의 시간이 자동 저장된다.
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate //조회한 Entity의 값이 변경될때의 시간을 저장합니다.
+    private LocalDateTime modifiedDate;
+}
--- src/main/java/net/ctblog/domain/posts/Posts.java
+++ src/main/java/net/ctblog/domain/posts/Posts.java
@@ -3,6 +3,7 @@ package net.ctblog.domain.posts;
 import lombok.Builder; //해당 클래스의 빌더 패턴 클래스 생성, 생성자 상다에 선언시 생성자에 포함된 필드(객체)만 빌더에 포함
 import lombok.Getter; //클래스내에 모든 필드에 Getter생성
 import lombok.NoArgsConstructor; //기본생성자 자동추가
+import net.ctblog.domain.BaseTimeEntity;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -13,7 +14,7 @@ import javax.persistence.Id;
 @Getter
 @NoArgsConstructor
 @Entity // 테이블과 링크될 클래스임을 나타낸다, 기본값으로 카멜케이스 이름을 언더스코어네이밍으로 테이블 이름과 매칭한다.
-public class Posts { //참고 : Entity 클래스에는 Setter를 만들지 않는다(인스턴스 값이 어디서 변하는지 구분이 어려워짐), 필요시 각각의 메소드를 추가한다.
+public class Posts extends BaseTimeEntity { //참고 : Entity 클래스에는 Setter를 만들지 않는다(인스턴스 값이 어디서 변하는지 구분이 어려워짐), 필요시 각각의 메소드를 추가한다.
 
     @Id //해당 테이블의 PK 필드를 뜻한다.
     @GeneratedValue(strategy = GenerationType.IDENTITY) //PK생성규칙을 나타낸다
