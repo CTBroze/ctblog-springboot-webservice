e8f7acfcd4d66aff60ef6551c4eca23281e99a6e
CTBroze <ctbroze@gmail.com>
2019-12-31 17:58:03 +0900

    Google로그인 기능 실습

---
+++ src/main/java/net/ctblog/domain/user/UserRepository.java
@@ -0,0 +1,7 @@
+package net.ctblog.domain.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email); //email을 통해 이미 생성된 사용자인지 확인하기위한 메서드
+}
--- src/main/java/net/ctblog/web/IndexController.java
+++ src/main/java/net/ctblog/web/IndexController.java
@@ -1,6 +1,8 @@
 package net.ctblog.web;
 
 import lombok.RequiredArgsConstructor;
+import net.ctblog.config.auth.LoginUser;
+import net.ctblog.config.auth.dto.SessionUser;
 import net.ctblog.service.PostsService;
 import net.ctblog.web.dto.PostsResponseDto;
 import org.springframework.stereotype.Controller;
@@ -8,14 +10,20 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model){
+    public String index(Model model, @LoginUser SessionUser user){
         model.addAttribute("posts",postsService.findAllDesc());
+        if(user != null){
+            model.addAttribute("userName",user.getName());
+        }
         return "index";
     }
 
