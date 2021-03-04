ec6408e9d53c566b5c99776c22ca1bb10e4b368c
Minseok Kim <ctbroze@gmail.com>
2021-03-04 22:53:22 +0900

    Spring Security Tag

--- src/main/java/com/ctbroze/web/IndexController.java
+++ src/main/java/com/ctbroze/web/IndexController.java
@@ -1,11 +1,21 @@
 package com.ctbroze.web;
 
 
+import com.ctbroze.config.auth.LoginUser;
+import com.ctbroze.config.auth.dto.SessionUser;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
-
+    @GetMapping("/")
+    public String index(Model model, @LoginUser SessionUser user){
+        if(user!=null){
+            model.addAttribute("userName",user.getName());
+        }
+        return "index";
+    }
 }
